ed8ddd84bf31679ba2f04989967ee3c8e7d8a3f8
Mario Reder <mario.reder@pm.me>
2026-04-18 21:23:07 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -18,7 +18,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_APP_DESCRIPTION": "Orderly Trading Application",
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
-  "VITE_ENABLED_MENUS": "Trading,Portfolio,Leaderboard,Vaults,Rewards",
+  "VITE_ENABLED_MENUS": "Trading,Portfolio,Leaderboard,Vaults,Rewards,Campaigns",
   "VITE_CUSTOM_MENUS": "NFT Marketplace, https://nft.orderlyx.io; Info Hub, https://orderlyx.blogspot.com/p/home.html",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
   "VITE_ENABLE_CAMPAIGNS": "false",
